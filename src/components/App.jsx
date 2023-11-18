8c885cdefbb3b6586b3645736c2813d87dcb7fe7
Annargb <annasamoylenko2015@gmail.com>
2023-11-18 19:54:28 +0100

    error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,9 +26,11 @@ export const App = () => {
     async function getImages() {
       try {
         let newRequest = request.split('').slice(14).join('');
+        setIsLoading(prevState => !prevState);
+        setFetchError(prevState => !prevState);
 
-        setIsLoading(true);
-        setFetchError(false);
+        // setIsLoading(true);
+        // setFetchError(false);
         setNewRequest(newRequest);
 
         const newImages = await fetchImages(newRequest, page);
@@ -36,14 +38,17 @@ export const App = () => {
         setImages(prevState => [...prevState, ...newImages.hits]);
         setTotal(newImages.totalHits);
       } catch (error) {
+        console.log(error);
+        setFetchError(prevState => !prevState);
+        // setFetchError(true);
         console.log(fetchError);
-        setFetchError(true);
         toast.error('Oops, Something went wrong! Try reloading the page!', {
           duration: 3500,
           position: 'top-right',
         });
       } finally {
-        setIsLoading(false);
+        setIsLoading(prevState => !prevState);
+        // setIsLoading(false);
       }
     }
 
@@ -89,92 +94,3 @@ export const App = () => {
     </Wrapper>
   );
 };
-
-// export class App extends Component {
-//   state = {
-//     request: '',
-//     newRequest: '',
-//     images: [],
-//     page: 1,
-//     error: false,
-//     isLoading: false,
-//     total: 0,
-//   };
-
-//   async componentDidUpdate(prevProps, prevState) {
-//     if (
-//       prevState.request !== this.state.request ||
-//       prevState.page !== this.state.page
-//     ) {
-//       try {
-//         let newRequest = this.state.request.split('').slice(14).join('');
-
-//         this.setState({ isLoading: true, error: false, newRequest });
-//         const newImages = await fetchImages(newRequest, this.state.page);
-
-//         this.setState(prevState => {
-//           return {
-//             images: [...prevState.images, ...newImages.hits],
-//             total: newImages.totalHits,
-//           };
-//         });
-//       } catch (error) {
-//         this.setState({ error: true });
-//         toast.error('Oops, Something went wrong! Try reloading the page!', {
-//           duration: 3500,
-//           position: 'top-right',
-//         });
-//       } finally {
-//         this.setState({ isLoading: false });
-//       }
-//     }
-//   }
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-//     const userRequest = event.target.request.value.trim();
-//     if (userRequest === '') {
-//       toast.error('Enter data in the field to search for images', {
-//         duration: 2500,
-//         position: 'top-right',
-//       });
-//       return;
-//     }
-//     let timeId = Date.now();
-//     this.setState({
-//       request: `${timeId}/${userRequest}`,
-//       page: 1,
-//       images: [],
-//     });
-//   };
-
-//   handleLoadMore = () => {
-//     this.setState(prevState => {
-//       return {
-//         page: prevState.page + 1,
-//       };
-//     });
-//   };
-
-//   render() {
-//     const { isLoading, images, total, request, newRequest } = this.state;
-
-//     return (
-//       <Wrapper>
-//         <Searchbar onSubmit={this.handleSubmit} />
-//         {request !== '' && !images.length && !isLoading && (
-//           <InfoMessage>
-//             We didn't find any images for request '{newRequest}'
-//           </InfoMessage>
-//         )}
-//         {images.length > 0 && <ImageGallery images={images} />}
-//         {isLoading && <Loader />}
-//         {0 < images.length && images.length < total && (
-//           <Button onClick={this.handleLoadMore} />
-//         )}
-//         <GlobalStyle />
-//         <Toaster />
-//       </Wrapper>
-//     );
-//   }
-// }
